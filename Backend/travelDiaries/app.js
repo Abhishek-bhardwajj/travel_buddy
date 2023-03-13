20f9b1eb11840ac265b07da62c3381a66d3133ab
Amit lavania <amit.lavania_cs20@gla.ac.in>
2023-03-14 01:06:44 +0530

    changes done

--- Backend/travelDiaries/app.js
+++ Backend/travelDiaries/app.js
@@ -1,6 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
 const app= express();
+dotenv.config();
 
 
+//middleware
 
-app.listen(5000,()=>console.log("Listening to local host port 5000"));
+// connection
+
+mongoose.connect(`mongodb+srv://admin:${process.env.MONGODB_PASSWORD}@cluster0.rstqcnt.mongodb.net/?retryWrites=true&w=majority`).then(()=>
+app.listen(5000,()=>
+console.log("Listening to local host port 5000")
+)
+).catch((err)=>console.log(err));
+
---
+++ Backend/travelDiaries/models/User.js
@@ -0,0 +1,22 @@
+import { Schema } from "mongoose";
+
+const userSchema=new Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+
+    },
+    password:{
+        type:String,
+            required:true,
+        minLength:6, 
+    },
+    posts:[{type:String}],
+
+});
+export default model("User",userSchema);
