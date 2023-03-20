cc0412d8047b587f469ecc7c9b56c346852dcb52
Manvi1108 <manvisinghal285@gmail.com>
2023-03-20 18:03:11 +0530

    app.js

--- Backend/travelDiaries/app.js
+++ Backend/travelDiaries/app.js
@@ -1,17 +1,21 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
-const app= express();
+import userRouter from "./routing/user-routes";
+const app = express();
 dotenv.config();
 
 
 //middleware
+app.use("/user", userRouter);
 
 // connection
 
-mongoose.connect(`mongodb+srv://admin:${process.env.MONGODB_PASSWORD}@cluster0.rstqcnt.mongodb.net/?retryWrites=true&w=majority`).then(()=>
-app.listen(5000,()=>
-console.log("Listening to local host port 5000")
-)
-).catch((err)=>console.log(err));
+mongoose
+    .connect(
+        `mongodb+srv://admin:${process.env.MONGODB_PASSWORD}@cluster0.8jtdpbh.mongodb.net/?retryWrites=true&w=majority`
+    )
+    .then(() =>
+        app.listen(5000,() => console.log("Listening & connecting to local host port 5000"))
+    ).catch((err)=>console.log(err));
 
