3bec7e31abbc55fec606bbd8eb90a627a1a52467
Manvi1108 <manvisinghal285@gmail.com>
2023-03-20 18:58:24 +0530

    req

--- Backend/travelDiaries/controller/user-controller.js
+++ Backend/travelDiaries/controller/user-controller.js
@@ -1,17 +1,16 @@
 import User from "../models/User";
-export const getAllUsers=async () =>
-{
-    let users;
-    try{
-        users=await User.find();
-    } catch(err){
-        console.log(err);
-    }
 
-    if(!users)
-    {
-        return res.status(500).json({message : "Unexpected Error Occured"});
-    }
+export const getAllUsers = async (req, res) => {
+  let users;
+  try {
+    users = await User.find();
+  } catch (err) {
+    return console.log(err);
+  }
 
-    return res.status(200).json({ users });
+  if (!users) {
+    return res.status(500).json({ message: "Unexpected Error Occured" });
+  }
+
+  return res.status(200).json({ users });
 };
