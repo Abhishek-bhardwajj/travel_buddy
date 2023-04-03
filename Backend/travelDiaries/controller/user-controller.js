cc2bd1ddec784fe71a75cd0daca0b09e052cd414
Manvi1108 <manvisinghal285@gmail.com>
2023-04-03 22:27:13 +0530

    getting user by id

--- Backend/travelDiaries/controller/user-controller.js
+++ Backend/travelDiaries/controller/user-controller.js
@@ -15,34 +15,51 @@ export const getAllUsers = async (req, res) => {
 
   return res.status(200).json({ users });
 };
-export const signup = async(req, res, next) => {
-    const {name, email, password} = req.body;
-    if (
-        !name &&
-        name.trim() === "" &&
-        !email &&
-        email.trim() === "" &&
-        !password &&
-        password.length < 6
-      ) {
-        return res.status(422).json({ message: "Inavalid Data" });
-      }
 
-      const hashedPassword = hashSync(password);
+export const getUserById = async (req, res) => {
+  const id = req.params.id;
 
-    let user;
-    try {
-        user = new User({ name, email, password: hashedPassword});
-        await user.save();
-    } catch (err) {
-        return console.log(err);
-    }
+  let user;
+  try {
+    user = await User.findById(id).populate("posts");
+  } catch (err) {
+    return console.log(err);
+  }
+  if (!user) {
+    return res.status(404).json({ message: "No user found" });
+  }
 
-    if (!user) {
-        return res.status(500).json({ message: "Unexpected Error Occured" });
-    }
+  return res.status(200).json({ user });
+};
 
-    return res.status(201).json({ user });
+export const signup = async (req, res, next) => {
+  const { name, email, password } = req.body;
+  if (
+    !name &&
+    name.trim() === "" &&
+    !email &&
+    email.trim() === "" &&
+    !password &&
+    password.length < 6
+  ) {
+    return res.status(422).json({ message: "Inavalid Data" });
+  }
+
+  const hashedPassword = hashSync(password);
+
+  let user;
+  try {
+    user = new User({ email, name, password: hashedPassword });
+    await user.save();
+  } catch (err) {
+    return console.log(err);
+  }
+
+  if (!user) {
+    return res.status(500).json({ message: "Unexpected Error Occured" });
+  }
+
+  return res.status(201).json({ user });
 };
 
 export const login = async (req, res, next) => {
--- Backend/travelDiaries/routing/user-routes.js
+++ Backend/travelDiaries/routing/user-routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getAllUsers, signup, login } from "../controller/user-controller";
+import { getAllUsers, signup, login, getUserById } from "../controller/user-controller";
 
 const userRouter = Router();
 
 userRouter.get("/", getAllUsers);
 userRouter.post("/signup", signup);
-userRouter.post("/login", login)
+userRouter.post("/login", login);
+userRouter.get("/:id", getUserById);
 
 export default userRouter;
